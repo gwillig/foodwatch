5063f9ebaa08801132cf6458c1461983e2584121
gwillig <gustavwillig@gmail.com>
2020-06-14 12:45:37 +0200

    MOD| Conver the data from the mysql db on heroku to the local db

--- analysis/Connect_heroku.py
+++ analysis/Connect_heroku.py
@@ -7,8 +7,7 @@ import os
 import json
 import pandas as pd
 from datetime import timezone
-
-
+from datetime import datetime
 
 
 
@@ -31,6 +30,10 @@ session = Session()
 result = db_con.execute("Select * from misc")
 for row in result:
     print(row)
+
+'#1.4.Step: convert data into csv'
+table = pd.read_sql_query("Select * from home_misc", db_con)
+table.to_csv("home_misc_14_06_20.csv")
 '#2.Step: Add new data to database'
 # db_con.execute("Insert value into table Misc(timestamp_unix,) VALUES()")
 
@@ -44,6 +47,21 @@ class Misc(Base):
     amount_weight = db.Column(db.Float)
     amount_steps = db.Column(db.Integer)
 
+class Food(Base):
+    __tablename__ = 'food'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    timestamp_unix = db.Column(db.Float)
+    timestamp_obj = db.Column(db.DateTime)
+    calorie = db.Column(db.Integer)
+
+
+    def __repr__(self):
+        '1.Step: Get all actors which were involved in move'
+        return f"{self.name};{self.timestamp_obj}"
+
+
 '''
 Example how to add data to db
 m1 = Misc(amount_steps=1000, timestamp_obj=datetime.utcfromtimestamp(1590451200),
@@ -54,26 +72,39 @@ session.commit()
 '''
 ##### Add data from data_19_04.csv to db
 
-df = pd.read_csv('analysis/data_19_04.csv')
+df = pd.read_csv('misc_14_06_20.csv')
 df_bulk = pd.DataFrame()
 '#1.Step: Pre-processing of df'
-
-df_without = df.drop(columns=["Calorie"])
-
+df = df.astype({'timestamp_unix': 'int64'})
 '#1.1.Step: Convert date string into date obj'
-df_bulk["timestamp_obj"] = df.Date.apply(lambda x:convert_str_date_obj_utc(x))
+df_bulk["timestamp_obj"] = df["timestamp_unix"].apply(lambda x:datetime.utcfromtimestamp((x)))
 '#1.2.Step: convert to unix'
 df_bulk["timestamp_unix"] = df_bulk.timestamp_obj.apply(lambda x: x.timestamp())
-df_bulk["amount_weight"] = df["Weight"]
-df_bulk["amount_steps"] = df["Steps."]
+df_bulk["amount_weight"] = df["amount_weight"]
+df_bulk["amount_steps"] = df["amount_steps"]
 
 '#2.Step: Add data from df_bulk to db'
 bulk_list = df_bulk.to_dict("row")
 bulk_list_obj = [Misc(**x) for x in bulk_list]
 session.bulk_save_objects(bulk_list_obj)
 session.commit()
+'########Insert food data'
+df = pd.read_csv('food_14_06_20.csv')
+df_bulk = pd.DataFrame()
+'#1.Step: Pre-processing of df'
+df = df.astype({'timestamp_unix': 'int64'})
+'#1.1.Step: Convert date string into date obj'
+df_bulk["timestamp_obj"] = df["timestamp_unix"].apply(lambda x:datetime.utcfromtimestamp((x)))
+'#1.2.Step: convert to unix'
+df_bulk["timestamp_unix"] = df_bulk.timestamp_obj.apply(lambda x: x.timestamp())
+df_bulk["amount_weight"] = df["amount_weight"]
+df_bulk["amount_steps"] = df["amount_steps"]
 
-
+'#2.Step: Add data from df_bulk to db'
+bulk_list = df_bulk.to_dict("row")
+bulk_list_obj = [Misc(**x) for x in bulk_list]
+session.bulk_save_objects(bulk_list_obj)
+session.commit()
 def convert_str_date_obj_utc(date_str):
     """
     Function converts a given string to a date obj with timezone utc
